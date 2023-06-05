c3216e48f79851fbc48c52e67319c7ba72a1e62a
David Rapoport <davirapo@gmail.com>
2023-06-05 04:21:40 -0300

    Agregando rutas

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import HomeDashboard from "../components/HomeDashboard"
+import TicketsDashboard from "../components/TicketsDashboard"
+import FlightResultsDashboard from "../components/FlightResultsDashboard"
 
 
 const RoutesConfig = () => {
   return (
     <Routes>
       <Route path="/" element={<HomeDashboard />} />
-      <Route path="/pasajes" />
+      <Route path="/pasajes" element={<TicketsDashboard />} />
+      <Route path="/resultados" element={<FlightResultsDashboard />} />
       <Route path="/usuarios" />
       <Route path="/rutas" />
       <Route path="/aeropuertos" />
